3a2585a520c4f2bbd355fc7eb849102f78ebb87c
kivilak <kivilakc@gmail.com>
2025-07-17 21:56:54 +0530

    feat: Implement CRUD operations for GuestManager

--- Event-Management/TestingModel/GuestManager.cs
+++ Event-Management/TestingModel/GuestManager.cs
@@ -11,7 +11,7 @@ namespace Event_Management.TestingModel
 
         }
 
-        public async Task<ObservableCollection<Guest>> GetAllGuests()
+        public async Task<ObservableCollection<Guest>> GetAllGuests()       // return all the guests
         {
             var guests = new ObservableCollection<Guest>();
             var db = DatabaseManager.Instance;
@@ -47,6 +47,59 @@ namespace Event_Management.TestingModel
             }
 
             return guests;
+        }
+
+        public async Task<bool> AddGuest(Guest guest)
+        {
+            var db = DatabaseManager.Instance;
+            var query = "INSERT INTO Guest(Name, Email, Phone, Category, RsvpStatus, Dietary, CheckedIn, EventId)" +
+                "VALUES(@Name, @Email, @Phone, @Category, @RsvpStatus, @Dietary, @CheckedIn, @EventId)";
+
+            try
+            {
+                using (var command = db.CreateCommand(query))
+                {
+                    command.Parameters.AddWithValue("@Name", guest.Name ?? "");
+                    command.Parameters.AddWithValue("@Email", guest.Email ?? "");
+                    command.Parameters.AddWithValue("@Phone", guest.Phone ?? "");
+                    command.Parameters.AddWithValue("@Category", guest.Category ?? "");
+                    command.Parameters.AddWithValue("@RsvpStatus", guest.RsvpStatus ?? "");
+                    command.Parameters.AddWithValue("@Dietary", guest.Dietary ?? "");
+                    command.Parameters.AddWithValue("@CheckedIn", guest.CheckedIn);
+                    command.Parameters.AddWithValue("@EventId", guest.EventId);
+
+                    int rowsAffected = await command.ExecuteNonQueryAsync();
+                    return rowsAffected > 0;
+                }
+            } 
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while adding guest: {ex.Message}");
+                return false;
+            }
+
+        }
+
+        public async Task<bool> DeleteGuest(int guestId)
+        {
+            var db = DatabaseManager.Instance;
+            var query = "DELETE FROM Guest WHERE GuestId = @GuestId";
+
+            try
+            {
+                using(var command = db.CreateCommand(query))
+                {
+                    command.Parameters.AddWithValue("@GuestId", guestId);
+
+                    int rowsAffected = await command.ExecuteNonQueryAsync();
+                    return rowsAffected > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while deleting guest: {ex.Message}");
+                return false;
+            }
         }
     }
 }
