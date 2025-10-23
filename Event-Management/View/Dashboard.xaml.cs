4f9c8fc22f1606b1228b2bd3ff01537c60a66139
ThilinaRodrigo <thilinalakshan2001@gmail.com>
2025-10-24 00:54:02 +0530

    added searching funactionality for dashboard and make few changes to add guest window

--- Event-Management/View/Dashboard.xaml.cs
+++ Event-Management/View/Dashboard.xaml.cs
@@ -1,31 +1,27 @@
 ﻿using Event_Management.TestingModel;
+using Event_Management.View.window;
+using Event_Management.ViewModel;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Event_Management.View
 {
-    /// <summary>
-    /// Interaction logic for Dashboard.xaml
-    /// </summary>
     public partial class Dashboard : UserControl
     {
+        // Optional callback to notify parent window when an event is selected
         internal Action<Event> EventSelected;
 
         public Dashboard()
         {
             InitializeComponent();
+            DataContext = new EventViewModel();
+        }
+
+        public void AddEventBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddEvent addEventWindow = new AddEvent();
+            addEventWindow.ShowDialog();
         }
     }
 }
