17cb60542a376ff29f39ca83f1f0c27a1952be76
ThilinaRodrigo <thilinalakshan2001@gmail.com>
2025-10-25 16:44:57 +0530

    added view model for budgets

---
+++ Event-Management/TestingModel/BudgetItem.cs
@@ -0,0 +1,24 @@
+﻿namespace Event_Management.Model
+{
+    public class BudgetItem
+    {
+        public string CategoryName { get; set; }
+        public string Event { get; set; }
+        public decimal Estimated { get; set; }
+        public decimal Actual { get; set; }
+
+        public decimal Difference => Actual - Estimated;
+
+        public string Status
+        {
+            get
+            {
+                if (Actual > Estimated) return "Over Budget";
+                if (Actual < Estimated) return "Under Budget";
+                return "On Budget";
+            }
+
+        }
+
+    }
+}
